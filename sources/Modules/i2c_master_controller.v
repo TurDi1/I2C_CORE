a0eaa4c8d949e3df81e3ec9db64f304d6bef5d78
TurDi1 <90939428+TurDi1@users.noreply.github.com>
2024-01-14 21:45:54 +0300

    Add files via upload

--- sources/Modules/i2c_master_controller.v
+++ sources/Modules/i2c_master_controller.v
@@ -38,7 +38,7 @@ inout          sda;           // Serial data inout
 //==================
 // Wire's, reg's etc
 reg				Sr_reg;
-reg				clear_;
+reg				clear_Sr_;
 
 reg      [2:0] state;
 
@@ -47,7 +47,10 @@ reg      [7:0] data_byte_reg;
 reg      [7:0] shift_reg;
 
 reg      [2:0] clock6x_counter;
+reg				clock6x_reset_;
 
+reg				scl_reg;
+reg				sda_reg;
 
 //==================
 // Parameters
@@ -57,20 +60,20 @@ parameter   start = 1;
 
 //==================
 // Assignments
-assign scl = bus_clock;
-assign busy = Sr_reg;
-
-//===================
-// Port logic section
+assign sda = sda_reg ? 1'bz : 1'b0;
+assign scl = scl_reg ? 1'bz : 1'b0;
 
 
 //==================
 // Sr storage logic
-always @ (posedge clk or negedge reset)
+always @ (posedge clk or negedge clear_Sr_)
 begin
-   if (!reset | !clear_)
-      Sr_reg	<= 0;
-   else if (clk)
+   if (!clear_Sr_)
+	begin
+		if (!Sr)
+			Sr_reg	<= 0;
+   end
+	else if (clk)
    begin
       if (Sr)
          Sr_reg	<= 1;
@@ -78,36 +81,57 @@ begin
          Sr_reg   <= Sr_reg;
    end      
    else
-      Sr_reg	<= Sr_reg;         
-   end      
+      Sr_reg	<= Sr_reg;    
 end
 
 //===================
 // FSM
-//always @ (posedge clk or negedge reset)
-//begin
-//   if (!reset)
-//      state <= idle;
-//   else
-//      case (state)
-//      // Idle state
-//      idle: begin
-//         
-//      end
-//      // start state
-//      start: begin
-//         
-//      end
-//      endcase
-//end
+always @ (posedge clk or negedge reset)
+begin
+   if (!reset)
+      state <= idle;
+   else
+      case (state)
+      // Idle state
+      idle: begin
+         clock6x_reset_	<= 0;
+			clear_Sr_		<= 1;	// In idle state we don't reset Sr flip-flop 
+			
+			sda_reg			<= 1;
+			scl_reg			<= 1;
+			
+			if (!empty_tx)
+			begin
+				state				<= start;
+				address_rw_reg	<= address_rw;	
+			end
+			else
+			begin
+				address_rw_reg	<= 0;
+				state				<= idle;
+			end				
+      end
+      // Start state
+      start: begin
+			clock6x_reset_	<= 1;
+			clear_Sr_		<= 1;
+
+         if ()
+         else
+         scl_reg        <= 1;
+         
+//         state				<= start;
+      end
+      endcase
+end
 
 //===========================
 // Counter logic for 6x clock
-always @ (posedge bus_clock6x or negedge reset)
+always @ (posedge bus_clock6x or negedge clock6x_reset_)
 begin
-   if (!reset)
+   if (!clock6x_reset_)
       clock6x_counter   <= 0;
-   else if (en)
+   else if (bus_clock6x)
       clock6x_counter   <= clock6x_counter + 1;
    else
       clock6x_counter   <= clock6x_counter;   
